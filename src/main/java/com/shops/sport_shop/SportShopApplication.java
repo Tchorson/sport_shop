1b7356253f328b21b3949783f01fc592e8988585
Tchorson <tchorek.mateusz@gmail.com>
2023-06-05 00:07:58 +0200

    initialize sport shop

---
+++ src/main/java/com/shops/sport_shop/SportShopApplication.java
@@ -0,0 +1,16 @@
+
+
+package com.shops.sport_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SportShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportShopApplication.class, args);
+	}
+
+}
